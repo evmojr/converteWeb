cca6e21da52abd57d5fa08e63b5fe09406a401ea
Evaldo <evmojr@hotmail.com>
2021-10-06 16:23:44 -0300

    files changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 process.title = 'MyWebServer';
 var args = process.argv,
-port = args[2] || 7070,
+port = process.env.PORT || args[2] || 7070,
 webServer = require('./server');
 
 webServer.listen(port, function() {
